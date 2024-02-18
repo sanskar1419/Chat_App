eeaca2d9bcd83b9b277c406f70a988654b3376c9
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-02-18 13:29:11 +0530

    SignUp user and save user data to db

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js";
+
 export default class AuthController {
   async login(req, res) {
     res.send("Welcome to Login Page");
@@ -7,10 +9,40 @@ export default class AuthController {
     }
   }
   async signUp(req, res) {
-    res.send("Welcome to Signup Page");
+    // console.log(req.body);
+    const { name, userName, password, gender } = req.body;
+    const user = await User.findOne({ userName });
+    if (user) {
+      return res.status(400).send("User Already Exist!!!!!!!");
+    }
+
+    const boyPic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlPic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+    const profilePic = gender === "Male" ? boyPic : girlPic;
+
+    const newUser = new User({
+      name,
+      userName,
+      password,
+      gender,
+      profilePic,
+    });
+
+    await newUser.save();
+    console.log(newUser);
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      userName: newUser.userName,
+      profilePic: newUser.profilePic,
+    });
+
     try {
     } catch (err) {
-      console.log(err);
+      console.log("Error in Sign Up Controller", err);
+      res.status(500).json({
+        error: "Internal Server Error",
+      });
     }
   }
   async logout(req, res) {
