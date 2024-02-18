53c6050e59f40584decd32035d05866a97f92c7a
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-02-18 11:07:39 +0530

    Connect to mongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes.js";
+import { connectUsingMongoose } from "./config/connectToMongoDB.js";
 
 const app = new express();
 dotenv.config();
@@ -13,5 +14,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
+  connectUsingMongoose();
   console.log(`Server is listening to port :: ${PORT}`);
 });
